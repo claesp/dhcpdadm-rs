c8235660248fc4bd299966c046d79a3a1a7e05d8
Claes Persson <c@cp.se>
2023-12-27 18:39:45 +0100

    Skeleton agents function.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod admin;
+mod api;
 
 use actix_web::{middleware::Logger, App, HttpServer};
 use admin::agent::get_agent;
+use api::agent::get_agents;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -14,6 +16,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(logger)
             .service(get_agent)
+            .service(get_agents)
     })
     .bind(("127.0.0.1", 8081))?
     .run()
