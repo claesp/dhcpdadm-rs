1484314687a445773ef2971d298eed41d62fc908
Claes Persson <c@cp.se>
2023-12-27 18:39:08 +0100

    Enforce ending slash.

--- src/admin/agent.rs
+++ src/admin/agent.rs
@@ -10,7 +10,7 @@ pub struct AgentIdentifier {
     agent_id: String
 }
 
-#[get("/admin/agent/{agent_id}")]
+#[get("/admin/agent/{agent_id}/")]
 pub async fn get_agent(agent_identifier: Path<AgentIdentifier>) -> Json<AgentIdentifier> {
     Json(agent_identifier.into_inner())
     /*Json(format!("not implemented: {}", agent_identifier.agent_id).to_string())*/
